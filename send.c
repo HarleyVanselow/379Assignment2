19707d1f31eb9d49503ddec4ff97f608ffb26f95
Main User Account <code@harleyv.net>
2016-11-01 20:53:31 -0600

    Now relays messages to all connected clients

--- send.c
+++ send.c
@@ -9,34 +9,31 @@ void* Send(void* input)
 		int write_buf_itr=0;
 		int read_buf_itr=0;
 
-		for(j=0; j<client_count;j++){
-			for(i =0; i<client_count;i++){
-				if(clients[i].buf[0] != 0){
-					char sender_name_length = strlen(clients[i].name);
-					short message_length = strlen(clients[i].buf);
+		for(i =0; i<client_count;i++){
+			if(clients[i].buf[0] != 0){
+				unsigned char sender_name_length = strlen(clients[i].name);
+				short message_length = strlen(clients[i].buf);
 
-					// printf("Writing message by %s\n",clients[i].name);
-					// printf("Message to write: %s\n",clients[i].buf);
-					// printf("Message length: %d\n",message_length);
-					// printf("Name length: %d\n",sender_name_length);
+				write_buf[write_buf_itr++]=0x00;
+				write_buf[write_buf_itr++]=sender_name_length; //Must be 1 byte
+				for(k=0;k<sender_name_length;k++){ // Write client name to outgoing buffer
+					write_buf[write_buf_itr++] = clients[i].name[k];
+				}
+				write_buf[write_buf_itr++] = message_length & 0xFF;
+				write_buf[write_buf_itr++] = message_length >> 8;
 
-					write_buf[write_buf_itr++]=0x00;
-					write_buf[write_buf_itr++]=sender_name_length; //Must be 1 byte
-					for(k=0;k<sender_name_length;k++){ // Write client name to outgoing buffer
-						write_buf[write_buf_itr++] = clients[i].name[k];
-					}
-					write_buf[write_buf_itr++] = message_length & 0xFF;
-    				write_buf[write_buf_itr++] = message_length >> 8;
-					for(k=0;k<message_length;k++){
-						write_buf[write_buf_itr++] = clients[i].buf[k];
-					}
-					printf("Buffer to send: %s\n",write_buf);
-					fflush(stdout);
-					send(clients[j].socket_id,clients[i].buf,256,0);//Shouldnt really be 256
-					memset(clients[i].buf,0,256);
+				
+				for(k=0;k<message_length;k++){
+					write_buf[write_buf_itr++] = clients[i].buf[k];
 				}
 				
-			}			
-		}
+				for(j=0;j<client_count;j++){
+					printf("Sending to: %d:%s\n",j,clients[j].name);
+					send(clients[j].socket_id,write_buf,write_buf_itr,0);	
+				}
+				memset(clients[i].buf,0,256);
+			}
+		}			
+		
 	}
 }
