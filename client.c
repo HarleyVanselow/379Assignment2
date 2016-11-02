4eff2a9e51acbbd0173f8156c86e0e54284c6b10
icydusk <kdanhillier@hotmail.co.uk>
2016-11-01 23:03:01 -0600

    client now exits on ctrl-c. Client displays messages from other clients

--- client.c
+++ client.c
@@ -10,8 +10,9 @@
 #include <syslog.h>
 #include <string.h>
 #include <pthread.h>
+#include <signal.h>
 
-int number_of_connected_users;
+int client_socket; 
 
 void * check_connection(unsigned char * buf){
     if (buf[0] == 207 && buf[1] == 167){
@@ -34,16 +35,18 @@ void * read_user_input(void * client_socket){
         fgets(message, 255, stdin);
         char message_to_send[256];
         snprintf(message_to_send, sizeof message_to_send, "%d%s", htons(strlen(message)), message);
-        // printf("sending message: %s\n", message_to_send);
-        if (strcmp(".quit\n\0", message) == 0){
-            exit(1);
-        } else {
-            printf("Sending: %s", message_to_send); fflush(stdout);
-            send(*((int *)client_socket),message_to_send,256,0);//Shouldnt really be 256
-            memset(message_to_send,0,256);
-        }
 
+        printf("Sending: %s", message_to_send); fflush(stdout);
+        send(*((int *)client_socket),message_to_send,256,0);//Shouldnt really be 256
+        memset(message_to_send,0,256);
     }
+}
+
+void close_client(int sig){
+    printf("Closing client\n");fflush(stdout);
+    close(client_socket);
+    shutdown(client_socket, 2);
+    exit(1);
 }
 
 void * send_username(void * client_socket, const char * username){
@@ -53,41 +56,47 @@ void * send_username(void * client_socket, const char * username){
         send(*((int *)client_socket),message_to_send,256,0);//Shouldnt really be 256
     }
 
-    int main(int argc, char const *argv[])
-    {
-        if (argc != 4){
-            printf("Invalid parameters!\n");
-            return -1;
-        }
+int main(int argc, char const *argv[]){
+    if (argc != 4){
+        printf("Invalid parameters!\n");
+        return -1;
+    }
 
-        const char * hostname = argv[1];
-        int port_number = atoi(argv[2]);
-        const char * username = argv[3];
-
-        int client_socket; 
-        uint32_t number;
-
-        unsigned char buf[256];
-
-        struct  sockaddr_in server;
-
-        struct  hostent     *host;
-
-        TAILQ_HEAD(tailhead, entry) head;
-        struct tailhead *headp;                
-        struct entry {
-         TAILQ_ENTRY(entry) entries;        
-     } *n1, *n2, *np;
-
-     TAILQ_INIT(&head);    
+    const char * hostname = argv[1];
+    int port_number = atoi(argv[2]);
+    const char * username = argv[3];
 
 
-     host = gethostbyname (hostname);
+    uint32_t number;
 
-     if (host == NULL) {
+    unsigned char buf[256];
+
+    struct  sockaddr_in server;
+
+    struct  hostent     *host;
+
+    TAILQ_HEAD(tailhead, entry) head;
+    struct tailhead *headp;                
+    struct entry {
+        TAILQ_ENTRY(entry) entries;        
+    } *n1, *n2, *np;
+
+    TAILQ_INIT(&head);    
+
+
+    host = gethostbyname (hostname);
+
+    if (host == NULL) {
         perror ("Client: cannot get host description");
         exit (1);
     }
+
+    struct sigaction onSigInt;
+    onSigInt.sa_handler = close_client;
+    sigemptyset(&onSigInt.sa_mask);
+    onSigInt.sa_flags = 1;
+
+    sigaction(SIGINT, &onSigInt, NULL);
 
 
     client_socket = socket (AF_INET, SOCK_STREAM, 0);
@@ -131,18 +140,27 @@ void * send_username(void * client_socket, const char * username){
     pthread_t user_input;
     pthread_create(&user_input, NULL, read_user_input, &client_socket);
 
+    int message_type;
+    int username_length;
+    uint16_t message_length;
+
     while (1){
 
-        int message_size = read (client_socket, &buf, 256);
-        int i = 2;
-        while (i < message_size){
-            if (buf[i] == '\0'){
-                printf(":");
-            } else {
-             printf("%c", buf[i]);
-         }
-         fflush(stdout);
-         i ++;
-     } 
- }
+        read (client_socket, &message_type, 1);
+        if (message_type == 0){
+
+            read(client_socket, &username_length, 1);
+            char sender_username[username_length];
+            read(client_socket, &sender_username, username_length);
+            sender_username[username_length] = '\0';
+            
+            read(client_socket, &message_length, 2);
+            char received_message[message_length];
+            read(client_socket, &received_message, message_length);
+            received_message[message_length] = '\0';
+
+            printf("%s: %s\n", sender_username, received_message);
+            fflush(stdout);
+        }
+    }
 }
