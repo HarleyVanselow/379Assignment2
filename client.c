46a62f92637b2bd3f23421fd1b833e68e0c408ab
Kdan <kdanhillier@hotmail.co.uk>
2016-11-04 12:17:30 -0600

    need to un network byte order

--- client.c
+++ client.c
@@ -83,7 +83,7 @@ void handle_message(){
     sender_username[username_length] = '\0';
 
     read(client_socket, &message_length, 2);
-    
+    message_length = htons(message_length);
     char * received_message = malloc (message_length+1);
     read(client_socket, received_message, message_length);
     received_message[message_length] = '\0';
